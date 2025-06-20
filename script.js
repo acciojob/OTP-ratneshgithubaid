5be8d0965ae66faa62526282e967d9d24e0aaea2
ratneshgithubaid <140415763+ratneshgithubaid@users.noreply.github.com>
2025-06-20 10:43:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', () => {
+    const codes = document.querySelectorAll('.code');
+    
+    codes[0].focus();
+    
+    codes.forEach((code, idx) => {
+        code.addEventListener('keydown', (e) => {
+            if(e.key >= 0 && e.key <= 9) {
+                codes[idx].value = '';
+                setTimeout(() => {
+                    if(idx < codes.length - 1) {
+                        codes[idx + 1].focus();
+                    }
+                }, 10);
+            } else if(e.key === 'Backspace') {
+                setTimeout(() => {
+                    if(idx > 0) {
+                        codes[idx - 1].focus();
+                    }
+                }, 10);
+            }
+        });
+    });
+});
